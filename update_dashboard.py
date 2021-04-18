354749b2945a630df98c4188d2085cd3e11e2c54
lubianat <tiago.lubiana.alves@usp.br>
2021-04-18 16:48:01 -0300

    code: Handle section order via yaml

--- update_dashboard.py
+++ update_dashboard.py
@@ -1,6 +1,47 @@
 from wbib import render_dashboard
+import yaml
+import queries as queries
+
+query_options = {
+  "map of institutions": {
+    "label": "Map of institutions",
+    "query": queries.get_query_url_for_locations() 
+  },
+  "100 most recent articles": {
+    "label": "100 most recent articles",
+    "query": queries.get_query_url_for_articles()
+  }, 
+  "list of authors": {
+    "label": "list of authors",
+    "query": queries.get_query_url_for_authors()
+  },
+  "list of topics": {
+    "label": "list of co-studied topics",
+    "query": queries.get_topics_as_table()
+  },
+  "list of journals":{
+    "label": "list of co-studied topics",
+    "query": queries.get_query_url_for_venues()  
+  }
+}
+
+
 
 with open("index.html", "w") as f:
-    html = render_dashboard()
+    with open("config.yaml") as f2:
+        config = yaml.load(f2, Loader=yaml.FullLoader)
+        print(config)
+
+    sections_to_add = []
+
+
+    for query_names in config["sections"]:
+        for key, value in query_names.items():
+            query_options[key]["label"] = value
+            print(value)
+            sections_to_add.append(key)
+
+
+    html = render_dashboard(config, query_options, sections_to_add)
     f.write(html)
 
--- wbib.py
+++ wbib.py
@@ -5,62 +5,19 @@ import unicodedata
 import queries as queries
 import render as render
 
-def render_dashboard():
+def render_dashboard(config, query_options, sections_to_add):
+  """
 
-  sections_to_add = ["map of institutions", 
-                   "100 most recent articles",
-                   "list of authors",
-                   "list of topics",
-                   "list of journals"]
+  - Args:
+     - config: A .yaml file with the configs
+  """
+  license_statement = config["license_statement"]
+  code_availability_statement = config["code_availability_statement"]
+  scholia_credit_statement = config["scholia_credit"]
+  creator_statement = config["creator_credit"]
+  site_title = config["title"]
+  site_subtitle = config["subtitle"]
 
-  query_options = {
-  "map of institutions": {
-    "label": "Map of institutions",
-    "query": queries.get_query_url_for_locations() 
-  },
-  "100 most recent articles": {
-    "label": "100 most recent articles",
-    "query": queries.get_query_url_for_articles()
-  }, 
-  "list of authors": {
-    "label": "list of authors",
-    "query": queries.get_query_url_for_authors()
-  },
-  "list of topics": {
-    "label": "list of co-studied topics",
-    "query": queries.get_topics_as_table()
-  },
-  "list of journals":{
-    "label": "list of co-studied topics",
-    "query": queries.get_query_url_for_venues()  
-  }
-}
-
-  license_statement = '''
-            This content is available under a <a target="_blank" href="https://creativecommons.org/publicdomain/zero/1.0/"> 
-            Creative Commons CC0</a> license.
-  '''
-  code_availability_statement = '''
-  Source code for the website available at <a target="_blank" href="https://github.com/lubianat/women_in_metabolomics">
-            https://github.com/lubianat/women_in_metabolomics </a>
-  '''
-
-  scholia_credit_statement = '''
-SPARQL queries adapted from <a target="_blank" href="https://scholia.toolforge.org/">Scholia</a>
-  '''
-
-  creator_statement = '''
- Based on a curation by <a target="_blank" href="https://legido-quigley-lab.com/"> Prof. Cristina Legido-Quigley</a> and collaborators at <a href="https://docs.google.com/document/d/1-GUNDm0ul0TRkkETYa6U1tjnMJdxwKRPqBftN2gGVAo/edit">
- this Google Docs </a>
- </p>
- <p>
- Dashboard  by <a target="_blank" href="https://www.wikidata.org/wiki/User:TiagoLubiana">TiagoLubiana</a>
-  '''
-
-  site_title = "Women in Metabolomics"
-  site_subtitle = '''A map via Wikidata from data curated in <a target="_blank" href="https://docs.google.com/document/d/1-GUNDm0ul0TRkkETYa6U1tjnMJdxwKRPqBftN2gGVAo/edit?usp=sharing">
-  this Google Docs </a>
-'''
   html = render.render_header(site_title) + \
   render.render_top(site_title, site_subtitle) +  \
   render.render_sections(sections_to_add, query_options) + \
